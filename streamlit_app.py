696a0084bb0bba2ea9fff1ecdf18072cc008f465
Harris Kiptoo <harykip@gmail.com>
2025-11-07 05:19:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 App Name')
 
 st.write('Hello world!')
 
 st.info('This app builds machine learning models')
-
+df= csv.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
 
