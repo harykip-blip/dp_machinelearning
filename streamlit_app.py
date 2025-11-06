231c99c4d15925c5d6ec8da4390f67a638cdb449
Harris Kiptoo <harykip@gmail.com>
2025-11-07 05:50:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,5 @@ st.write('**y**')
 y=df.species
 y
 with st.expander ('Data visualization'):
-  # "species","island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
+  
   st.scatter_chart(data=df,X="bill_length_mm", y="body_mass_g", color= "species") 
