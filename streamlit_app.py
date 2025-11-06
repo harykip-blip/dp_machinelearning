caaaee444c292c3c256c6c4b6fb228967f17fe2e
Harris Kiptoo <harykip@gmail.com>
2025-11-07 05:20:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.title('🎈 App Name')
 st.write('Hello world!')
 
 st.info('This app builds machine learning models')
-df= csv.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
+df=pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
 df
 
