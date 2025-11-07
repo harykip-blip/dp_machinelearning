fd096d6cd47c4974cf92c3e7d0be6a77bf4d9b2c
Harris Kiptoo <harykip@gmail.com>
2025-11-07 16:12:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,9 @@ data= {'island':island,
        'bill_depth_mm': bill_depth_mm,
        'flipper_length_mm': flipper_length_mm,
        'body_mass_g': body_mass_g,
-       ' gender' :  gender }
+       ' gender' :  gender 
+      
+}
 
 input_df=pd.DataFrame(data, index[0])
 input_penguins= pd.concat([input_df, x], axis=0)
